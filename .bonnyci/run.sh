7f967342db008877514a5357d129f4d44a101ca7
Adam Gandelman <adamg@ubuntu.com>
2017-05-02 11:23:10 -0700

    Test commit
     * .bonnyci/run.sh: Set: fail_gate=1 test_output=XXX TEST_ID: 0785ae12-960b-44a1-ad67-0d7efe16a3ed fail_check=0

--- .bonnyci/run.sh
+++ .bonnyci/run.sh
@@ -1,6 +1,14 @@
-#!/bin/bash -xe
+#!/bin/bash
 
-echo $BONNYCI_TEST_PIPELINE > $BONNYCI_TEST_LOG_DIR/pipeline
-dmesg >$BONNYCI_TEST_LOG_DIR/dmesg
-sleep 30
-echo "True"
+FAIL_CHECK=0
+FAIL_GATE=1
+TEST_OUTPUT="XXX TEST_ID: 0785ae12-960b-44a1-ad67-0d7efe16a3ed"
+
+echo "$TEST_OUTPUT"
+
+case "$BONNYCI_TEST_PIPELINE" in
+    "check") [[ "$FAIL_CHECK" == 1 ]] && exit 1 ;;
+    "gate") [[ "$FAIL_GATE" == 1 ]] && exit 1 ;;
+esac
+
+exit 0
